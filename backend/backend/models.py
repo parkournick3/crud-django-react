ceb22644717f5a18edfb042904f33f9727770aab
nicolas <nicolasalexandre0001@gmail.com>
2024-02-29 08:20:09 -0300

    create virtual env and add post model

---
+++ backend/backend/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+  id = models.AutoField(primary_key=True)
+  username = models.CharField(max_length=100)
+  created_datetime = models.DateTimeField(auto_now_add=True)
+  content = models.TextField()
+
+  def __str__(self):
+    return self.username + ': ' + self.content
