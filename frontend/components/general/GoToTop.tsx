a4b434d77eb59f76cbbb4d94d4f676a6ad34c6b7
nicolas <nicolasalexandre0001@gmail.com>
2024-03-02 11:51:08 -0300

    hide go to top button when at top

--- frontend/components/general/GoToTop.tsx
+++ frontend/components/general/GoToTop.tsx
@@ -1,6 +1,31 @@
+"use client";
+
 import { ArrowUpIcon } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const GoToTop = () => {
+  const [scrollPosition, setSrollPosition] = useState(0);
+  const [showGoTop, setshowGoTop] = useState(false);
+
+  const handleVisibleButton = () => {
+    const position = window.scrollY;
+    setSrollPosition(position);
+
+    if (scrollPosition > 50) {
+      return setshowGoTop(true);
+    } else if (scrollPosition < 50) {
+      return setshowGoTop(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleVisibleButton);
+  });
+
+  if (!showGoTop) {
+    return null;
+  }
+
   return (
     <a
       href="#"
